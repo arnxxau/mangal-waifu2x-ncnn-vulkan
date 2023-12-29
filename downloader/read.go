82cc9cc973f2de6ae88ec11a1b3fd0e5e03cae64
arnxxau <59172081+arnxxau@users.noreply.github.com>
2023-12-29 23:12:16 +0100

    waifu2x passthrough

--- downloader/read.go
+++ downloader/read.go
@@ -1,6 +1,7 @@
 package downloader
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/metafates/mangal/color"
 	"github.com/metafates/mangal/constant"
@@ -12,10 +13,65 @@ import (
 	"github.com/metafates/mangal/source"
 	"github.com/metafates/mangal/style"
 	"github.com/spf13/viper"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
 )
+
+// UpscaleImage uses waifu2x-ncnn-vulkan to upscale an image
+func UpscaleImage(inputPath, outputPath string) error {
+	cmd := exec.Command("waifu2x-ncnn-vulkan", "-i", inputPath, "-o", outputPath, "-n", "3", "-s", "4")
+	// Add other options as needed
+
+	// Create buffers to capture standard output and standard error
+	var stdoutBuf, stderrBuf bytes.Buffer
+	cmd.Stdout = &stdoutBuf
+	cmd.Stderr = &stderrBuf
+
+	// Run the command
+	err := cmd.Run()
+
+	// Output the results
+	fmt.Printf("stdout: %s\n", stdoutBuf.String())
+	fmt.Printf("stderr: %s\n", stderrBuf.String())
+
+	return err
+}
+
+// bufferToFile writes the contents of a bytes.Buffer to a temporary file and returns the file path
+func bufferToFile(buf *bytes.Buffer, extension string) (string, error) {
+	// Create a temporary file
+	tmpFile, err := ioutil.TempFile("", "page-*."+extension)
+	if err != nil {
+		return "", err
+	}
+	defer tmpFile.Close()
+
+	// Write buffer to file
+	if _, err := buf.WriteTo(tmpFile); err != nil {
+		return "", err
+	}
+
+	return tmpFile.Name(), nil
+}
+
+// fileToBuffer reads the contents of a file into a bytes.Buffer
+func fileToBuffer(filePath string) (*bytes.Buffer, error) {
+	// Read file contents
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a buffer and write data to it
+	buf := bytes.NewBuffer(data)
+	return buf, nil
+}
 
 // Read the chapter by downloading it with the given source
 // and opening it with the configured reader.
+
 func Read(chapter *source.Chapter, progress func(string)) error {
 	if viper.GetBool(key.ReaderReadInBrowser) {
 		return open.StartWith(
@@ -44,6 +100,33 @@ func Read(chapter *source.Chapter, progress func(string)) error {
 	if err != nil {
 		log.Error(err)
 		return err
+	}
+
+	for _, page := range chapter.Pages {
+		// Write the page's contents to a temporary file
+		inputPath, err := bufferToFile(page.Contents, page.Extension)
+		if err != nil {
+			return err // Return early if there's an error
+		}
+		defer os.Remove(inputPath) // Schedule cleanup of the temp file
+
+		// Define the output path for the upscaled image
+		outputPath := strings.TrimSuffix(inputPath, ".jpeg") + "_upscaled..jpeg"
+
+		// Upscale the image
+		if err := UpscaleImage(inputPath, outputPath); err != nil {
+			return err // Return early if there's an error
+		}
+
+		// Read the upscaled image back into the page's Contents
+		upscaledBuffer, err := fileToBuffer(outputPath)
+		if err != nil {
+			return err // Return early if there's an error
+		}
+		defer os.Remove(outputPath) // Schedule cleanup of the upscaled file
+
+		// Update the page's Contents with the upscaled image
+		page.Contents = upscaledBuffer
 	}
 
 	log.Info("getting " + viper.GetString(key.FormatsUse) + " converter")
